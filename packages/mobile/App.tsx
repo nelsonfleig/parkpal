3a10a148d6ca1596f56b437609080045437bfe98
brandon <bdickson1212@gmail.com>
2022-01-30 15:28:18 +0100

    feat: installed react native paper and initial setup done with fonts and main colours

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,12 +1,28 @@
+/* eslint-disable global-require */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { useFonts } from 'expo-font';
+import theme from './styles/theme';
 
 export default function App() {
+  const [loaded] = useFonts({
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    InterThin: require('./assets/fonts/Inter-Thin.ttf'),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Button icon="camera">Press Me</Button>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
---
+++ packages/mobile/styles/fonts.ts
@@ -0,0 +1,22 @@
+const fontConfig = {
+  ios: {
+    regular: {
+      fontFamily: 'InterRegular',
+      fontWeight: 'normal' as 'normal',
+    },
+    medium: { fontFamily: 'InterMedium', fontWeight: 'normal' as 'normal' },
+    light: { fontFamily: 'inter-light', fontWeight: 'normal' as 'normal' },
+    thin: { fontFamily: 'inter-thin', fontWeight: 'normal' as 'normal' },
+  },
+  android: {
+    regular: {
+      fontFamily: 'inter-regular',
+      fontWeight: 'normal' as 'normal',
+    },
+    medium: { fontFamily: 'inter-medium', fontWeight: 'normal' as 'normal' },
+    light: { fontFamily: 'inter-light', fontWeight: 'normal' as 'normal' },
+    thin: { fontFamily: 'inter-thin', fontWeight: 'normal' as 'normal' },
+  },
+};
+
+export default fontConfig;
