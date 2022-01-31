6775d172d3a713893de6450a29a267901953a276
Viktor <vi.h.seo@gmail.com>
2022-01-31 11:45:10 +0100

    Setup the theme, customized default button

--- packages/web/src/components/testingButton.tsx
+++ packages/web/src/components/testingButton.tsx
@@ -2,19 +2,32 @@ import { Button, Paper, Box } from '@mui/material';
 import { styled } from '@mui/system';
 import { withTheme } from '@material-ui/styles';
 
-const StyledButton = styled(Button)`
-  background-color: ${(props) => props.theme.palette.primary.main};
-`;
+// const StyledButton = styled(Button)`
+//   background-color: ${(props) => props.theme.palette.primary.main};
+//   &:hover {
+//     background-color: #a9a9a9;
+//   }
+// `;
 
-// export const StyledButton = styled(withTheme(Button))((props) => ({
-//   background: props.theme.palette.primary.main,
-//   color: props.theme.palette.secondary.main,
-// }));
+export const StyledButton = styled(withTheme(Button))((props) => ({
+  background: props.theme.palette.primary.main,
+  ':hover': {
+    backgroundColor: props.theme.palette.primary.light,
+  },
+}));
+
+export const AnotherButton = styled(withTheme(Button))((props) => ({
+  background: props.theme.palette.secondary.main,
+  ':hover': {
+    backgroundColor: props.theme.palette.secondary.light,
+  },
+}));
 
 const StyledButtons = () => {
   return (
     <div>
-      <StyledButton>rgoerjgo</StyledButton>
+      <StyledButton>First Button</StyledButton>
+      <AnotherButton>Another Button</AnotherButton>
     </div>
   );
 };
