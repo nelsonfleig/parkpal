71dc64643b37cf1f167c3417c5e32ad00905d37a
brandon <bdickson1212@gmail.com>
2022-01-31 09:25:16 +0100

    fix: add autocomplete to text inputs to remove TS error

--- packages/mobile/src/screens/login/login.tsx
+++ packages/mobile/src/screens/login/login.tsx
@@ -4,34 +4,41 @@ import { View } from 'react-native';
 import { StartScreen } from '../../components/StartScreen/startScreen';
 import styles from './loginStyles';
 import { CustomButton } from '../../components/forms/button';
-import { WelcomeProps } from '../../../types/rootStack';
 
-export const LoginScreen = ({ navigation }: WelcomeProps) => {
+export const LoginScreen = () => {
   const { colors } = useTheme();
-  const login = () => {
-    navigation.navigate('Login');
-  };
+
   return (
     <StartScreen>
       <View style={styles.container}>
-        <Formik>
-          <View>
-            <TextInput
-              label="Email"
-              mode="outlined"
-              style={styles.input}
-              theme={{ colors: { primary: '#0A2540' } }}
-            />
-            <TextInput
-              label="Password"
-              mode="outlined"
-              style={styles.input}
-              theme={{ colors: { primary: '#0A2540' } }}
-            />
-            <CustomButton press={login} bg="#fff" color={colors.primary}>
-              Log In
-            </CustomButton>
-          </View>
+        <Formik initialValues={{ email: '', password: '' }} onSubmit={() => {}}>
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View>
+              <TextInput
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                label="Email"
+                mode="outlined"
+                style={styles.input}
+                theme={{ colors: { primary: '#0A2540', text: 'black' } }}
+                autoComplete="email"
+              />
+              <TextInput
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                label="Password"
+                mode="outlined"
+                style={styles.input}
+                theme={{ colors: { primary: '#0A2540', text: 'black' } }}
+                autoComplete="password"
+              />
+              <CustomButton press={handleSubmit} bg="#fff" color={colors.primary}>
+                Log In
+              </CustomButton>
+            </View>
+          )}
         </Formik>
       </View>
     </StartScreen>
