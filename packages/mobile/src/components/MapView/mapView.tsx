bb8eddf99f608d914583382ae9a1cd7321e39cec
Guillem Sardà <gsardaparreu@gmail.com>
2022-02-01 12:17:50 +0100

    feat: Add search location

--- packages/mobile/src/components/MapView/mapView.tsx
+++ packages/mobile/src/components/MapView/mapView.tsx
@@ -1,29 +1,68 @@
-import MapView, { Circle, Marker, Polyline } from 'react-native-maps';
+import { LocationGeocodedLocation } from 'expo-location';
+import MapView, { Circle, Marker } from 'react-native-maps';
+import { View } from 'react-native';
+import { useEffect, useState } from 'react';
+// import MapViewDirections from 'react-native-maps-directions';
 
+// import { API_DIRECTIONS_KEY } from '@env';
 import { mapViewStyles } from './mapViewStyles';
 
-export const MapComponent = ({ latitude, longitude }: { latitude: number; longitude: number }) => (
-  <MapView
-    provider="google"
-    style={mapViewStyles.map}
-    initialRegion={{
-      latitude,
-      longitude,
-      latitudeDelta: 0.001,
-      longitudeDelta: 0.01,
-    }}>
-    <Marker coordinate={{ latitude, longitude }} pinColor="blue" />
-    <Circle
-      center={{ latitude, longitude }}
-      radius={400}
-      strokeColor="blue"
-      fillColor="rgba(126, 113, 233, 0.3)"
-    />
-    <Polyline
-      coordinates={[
-        { latitude, longitude },
-        { latitude: 41.39912511303243, longitude: 2.1943598288348753 },
-      ]}
-    />
-  </MapView>
-);
+type MapComponentProps = {
+  latitude: number;
+  longitude: number;
+  destination: LocationGeocodedLocation | null;
+};
+
+export const MapComponent = ({ latitude, longitude, destination }: MapComponentProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [origin, setOrigin] = useState({ latitude, longitude });
+  const [mapDest, setMapDest] = useState(null as LocationGeocodedLocation | null);
+
+  useEffect(() => {
+    setOrigin({ latitude, longitude });
+    setMapDest(destination);
+  }, [latitude, longitude, destination]);
+
+  return (
+    <MapView
+      provider="google"
+      style={mapViewStyles.map}
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      showsUserLocation
+      followsUserLocation
+      showsMyLocationButton
+      region={
+        mapDest
+          ? {
+              latitude: mapDest.latitude,
+              longitude: mapDest.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }
+          : undefined
+      }>
+      {mapDest && (
+        <View>
+          <Marker coordinate={{ latitude: mapDest.latitude, longitude: mapDest.longitude }} />
+          <Circle
+            center={{ latitude: mapDest.latitude, longitude: mapDest.longitude }}
+            radius={300}
+            fillColor="rgba(126, 113, 233, 0.3)"
+          />
+        </View>
+      )}
+      {/* <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={API_DIRECTIONS_KEY} // insert your API Key here
+          strokeWidth={10}
+          strokeColor="#111111"
+        /> */}
+    </MapView>
+  );
+};
--- packages/mobile/src/screens/Landing/landing.tsx
+++ packages/mobile/src/screens/Landing/landing.tsx
@@ -1,14 +1,18 @@
-import { View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { useEffect, useState } from 'react';
-import { LocationObject } from 'expo-location';
+import { View, Text } from 'react-native';
+import { LocationGeocodedLocation, LocationObject } from 'expo-location';
 import * as Location from 'expo-location';
 
+import { Searchbar } from 'react-native-paper';
 import { landingStyles } from './landingStyles';
 import { MapComponent } from '../../components/MapView/mapView';
 
 export const LandingScreen = () => {
   const [location, setLocation] = useState(null as LocationObject | null);
+  const [destination, setDestination] = useState(null as LocationGeocodedLocation | null);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const onChangeSearch = (query: string) => setSearchQuery(query);
 
   useEffect(() => {
     (async () => {
@@ -17,18 +21,47 @@ export const LandingScreen = () => {
         return;
       }
 
-      const place = await Location.getLastKnownPositionAsync();
-      setLocation(place);
+      await Location.watchPositionAsync(
+        {
+          distanceInterval: 100,
+        },
+        (locObj) => {
+          // eslint-disable-next-line no-console
+          console.log(locObj);
+          setLocation(locObj);
+        }
+      );
     })();
   }, []);
 
   return (
-    <SafeAreaView style={landingStyles.container}>
+    <View style={landingStyles.container}>
+      <Searchbar
+        autoComplete
+        placeholder="Enter a destination"
+        iconColor="#7145D6"
+        theme={{ colors: { text: 'black' } }}
+        style={{ position: 'absolute', top: 60, right: 20, left: 20, zIndex: 3 }}
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        onSubmitEditing={async () => {
+          const temp = await Location.geocodeAsync(searchQuery);
+          // eslint-disable-next-line no-console
+          console.log(temp[0]);
+          setDestination(temp[0]);
+        }}
+      />
       <View>
         {location ? (
-          <MapComponent latitude={location.coords.latitude} longitude={location.coords.longitude} />
-        ) : null}
+          <MapComponent
+            latitude={location.coords.latitude}
+            longitude={location.coords.longitude}
+            destination={destination}
+          />
+        ) : (
+          <Text>Warm lentils alert! You have to enable the location to use ParkPal.</Text>
+        )}
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
