f728505569f6032754a55d2ec2298a37ac766985
Guillem Sardà <gsardaparreu@gmail.com>
2022-02-02 19:11:40 +0100

    fix(landing): remove markers properly
    When we did not delete the whole query and search for another one, the markers from the initial
    query were kept in the map.

--- packages/mobile/src/screens/Landing/landing.tsx
+++ packages/mobile/src/screens/Landing/landing.tsx
@@ -4,21 +4,20 @@ import { LocationGeocodedLocation, LocationObject } from 'expo-location';
 import * as Location from 'expo-location';
 
 import { Searchbar } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { landingStyles } from './landingStyles';
 import { MapComponent, mapRef } from '../../components/MapView/mapView';
 import { BookingPopup } from '../../components/BookingPopup/bookingPopup';
 
 export const LandingScreen = () => {
-  const [location, setLocation] = useState(null as LocationObject | null);
-  const [destination, setDestination] = useState(null as LocationGeocodedLocation | null);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [location, setLocation] = useState(null as LocationObject | null); // Here we get the user's current location
+  const [searchQuery, setSearchQuery] = useState(''); // We set the query on the searchbar
+  const [destination, setDestination] = useState(null as LocationGeocodedLocation | null); // Here we set the destination chosen by the user with the prev query
 
   const onChangeSearch = (query: string) => {
+    setSearchQuery(query);
     if (query === '') {
-      setDestination(null);
-      setSearchQuery('');
-    } else {
-      setSearchQuery(query);
+      setDestination(null); // Needed to remove the markers when we delete the text
     }
   };
 
@@ -34,8 +33,6 @@ export const LandingScreen = () => {
           distanceInterval: 100,
         },
         (locObj) => {
-          // eslint-disable-next-line no-console
-          console.log(locObj);
           setLocation(locObj);
         }
       );
@@ -57,10 +54,11 @@ export const LandingScreen = () => {
           // When we submit the direction, we transform it to coordinates
           const locationQuery = searchQuery ? await Location.geocodeAsync(searchQuery) : null;
           if (locationQuery) {
+            setDestination(null); // We first remove all the markers from the map
             mapRef.current?.animateCamera(
               {
                 center: {
-                  latitude: locationQuery[0].latitude,
+                  latitude: locationQuery[0].latitude, // We select the first object of the array
                   longitude: locationQuery[0].longitude,
                 },
                 heading: 0,
@@ -70,7 +68,7 @@ export const LandingScreen = () => {
               },
               { duration: 1000 }
             );
-            setDestination(locationQuery[0]); // We select the first object of the array
+            setDestination(locationQuery[0]); // We set new markers in the map
           }
         }}
       />
@@ -82,7 +80,9 @@ export const LandingScreen = () => {
             destination={destination}
           />
         ) : (
-          <Text>Warm lentils alert! You have to enable the location to use ParkPal.</Text>
+          <SafeAreaView>
+            <Text>Warm lentils alert! You have to enable the location to use ParkPal.</Text>
+          </SafeAreaView>
         )}
       </View>
       <BookingPopup />
