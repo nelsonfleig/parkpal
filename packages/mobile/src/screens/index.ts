b49733ee79d63789defaf1d03e650053b1d8c822
Viktor <vi.h.seo@gmail.com>
2022-02-08 12:59:17 +0100

    fix/navbarLetterShowing

--- packages/mobile/src/screens/index.ts
+++ packages/mobile/src/screens/index.ts
@@ -1,4 +1,4 @@
-// export * from './Welcome/welcome';
+export * from './Welcome/welcome';
 export * from './Login/login';
 export * from '../navigators/home'; // NAVIGATION STACK
 export * from './Register/register';
