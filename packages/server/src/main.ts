226f49d489fbf0503a5f44bb3adff03c364ae7bc
Nelson Fleig <nfleig@hotmail.com>
2022-02-08 22:17:29 +0100

    fix: included credentials in prod

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: process.env.NODE_ENV === 'production',
+    credentials: true,
   });
   app.use(cookieParser());
   app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
